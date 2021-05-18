62297c8cbfdac9320b81b521438b5a1642dac121
clip <binderclip2014@gmail.com>
2021-05-18 13:50:08 +0800

    update gen sh

--- proto3/gen_go.sh
+++ proto3/gen_go.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-protoc -I=./ --go_out=./demo ./demo.proto
+protoc -I=./ --go_out=./demo --go_opt=paths=source_relative ./demo.proto
+
+# https://stackoverflow.com/questions/61666805/correct-format-of-protoc-go-package
+# just add --go_opt=paths=source_relative
